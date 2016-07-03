0372a7090bcc50d8d021298ee5ae62341604b590
Enteez Dev <enteez.git@gmail.com>
2016-07-03 21:18:12 +0530

    Updated to test Jenkins automatic build

--- HelloWorld/src/main/java/org/psl/devops/App.java
+++ HelloWorld/src/main/java/org/psl/devops/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "This is updated on 3rd July 21:17" );
     }
 }
